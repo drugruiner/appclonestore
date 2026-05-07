3c1334932b487d5626f5421fb4136a0eb5c69e96
drugruiner <alinkapower93@gmail.com>
2026-05-07 19:03:34 +0300

    Load hero card layout fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './ios-share.css';
+import './hero-card-fix.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
