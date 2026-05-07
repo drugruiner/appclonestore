c134e8d61d82b2ffecc150d21aad848a1e44824e
drugruiner <alinkapower93@gmail.com>
2026-05-07 19:11:44 +0300

    Load App Store screenshot styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './ios-share.css';
 import './hero-card-fix.css';
+import './app-screenshots.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
