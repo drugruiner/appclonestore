46b6b4d8ab039b5cb26b09b465f00520cc7abcc9
drugruiner <alinkapower93@gmail.com>
2026-05-07 20:12:29 +0300

    Enable scroll reactive tab bar motion

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './appstore-meta.css';
 import './reviews.css';
 import './remove-account.css';
 import './liquid-tabbar.css';
+import './tabbar-scroll';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
